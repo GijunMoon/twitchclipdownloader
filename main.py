1244b49c9dc52e0574a3ba78fc4452655e0e0674
GijunMoon (기준 문) <gijunmoon@moonstudio.kro.kr>
2020-02-11 20:35:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-userurl = input("다운로드 하려는 클립의 주소를 입력하세요")
+userurl = input("다운로드 하려는 클립의 주소를 입력하세요") #주소 입력받기
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -10,7 +10,7 @@ options.add_argument('window-size=1920x1080')
 # options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
 driver = webdriver.Chrome('chromedriver' , chrome_options=options)
-driver.get(userurl) #특정 클립 링크
+driver.get(userurl) #입력받은 클립 링크
 
 time.sleep(3)
 
@@ -19,7 +19,7 @@ url_element = driver.find_element_by_tag_name('video')
 vid_url = url_element.get_attribute('src')
 print(vid_url)
 
-#클립 제목과 날짜 확인
+#클립 제목, 날짜 확인
 title_element1 = driver.find_element_by_class_name('tw-flex')
 title_element2 = title_element1.find_elements_by_tag_name('span')
 vid_title,vid_date = None, None
@@ -35,7 +35,7 @@ for span in title_element2:
 
 print(vid_title,'\t',vid_date)
 
-#특수문자 없애고 빈칸도 없에기
+#특수문자, 빈칸 제거
 import re
 vid_title = re.sub('[^0-9a-zA-Zㄱ-힗]', '', vid_title)
 vid_date = re.sub('[^0-9a-zA-Zㄱ-힗]', '', vid_date)
